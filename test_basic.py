4222e67ab5d8a68582f580da4764dab8d4338681
Eddy Martinez <215871462+booknest601@users.noreply.github.com>
2026-01-23 05:43:31 -0500

    Add basic tests for Local File Organizer

--- test_basic.py
+++ test_basic.py
@@ -3,11 +3,9 @@ Basic tests for Local File Organizer
 """
 import os
 
-
 def test_import():
     """Test that basic imports work"""
     assert True
-
 
 def test_environment():
     """Test that the environment is set up correctly"""
